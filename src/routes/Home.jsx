d337273ed23aa070b06a610d366c61de711bd6e5
Adrian Wysocki <a.wysocki@uniqsoft.pl>
2025-01-30 11:30:46 +0100

    Prepare home page

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,32 @@
+import useSWR from "swr";
+import { Flex, Spinner, Alert } from "@chakra-ui/react";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const {
+    data: products,
+    error,
+    isLoading,
+  } = useSWR("/products?sort=asc&limit=4");
+
+  return (
+    <Flex justifyContent={"center"} alignItems={"center"}>
+      {isLoading && <Spinner size="2xl" />}
+
+      {error && (
+        <Alert.Root status="error" maxWidth={"30%"}>
+          <Alert.Indicator />
+          <Alert.Content>
+            <Alert.Title>Server Error</Alert.Title>
+            <Alert.Description>Sorry for that...</Alert.Description>
+          </Alert.Content>
+        </Alert.Root>
+      )}
+
+      {products?.map((product) => (
+        <p key={product.id}>{product.title}</p>
+      ))}
+    </Flex>
+  );
 };
 
 export default Home;
